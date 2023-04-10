0d8b4d5dbb2c0bb66c5df3367bc90e6b8c5bc210
LEO GOUBARD <goubardleo@gmail.com>
2023-04-10 20:15:59 +0200

    :bug: i don't understand why my req.body is empty..

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -16,23 +16,23 @@ router.route('/').get((req, res) => {
   res.send('Hello from Dalle')
 })
 
-router.route('/').post(async(req, res) => {
+router.route('/').post(async (req, res) => {
   try {
+    console.log('prompt: ', req.body)
     const { prompt } = req.body;
-
-    const aiReponse = await openai.createImage({
-      prompt: prompt,
+    const aiResponse = await openai.createImage({
+      prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json'
     });
 
-    const image = aiReponse.data.data[0].b64_json;
+    const image = aiResponse.data.data[0].b64_json;
 
     res.status(200).json({ photo: image })
   } catch(err) {
-    console.log(err)
-    res.status(500).send(err?.reponse.data.error.message);
+    console.log(err?.response.data.error.message);
+    res.status(500).send(err?.response.data.error.message);
   }
 })
 
